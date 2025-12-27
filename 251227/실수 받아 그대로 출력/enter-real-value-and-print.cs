f325c660f4f377e6924f27d4cb8b2e0d112f7399
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-27 20:03:09 +0900

    Title: 실수 받아 그대로 출력, Time: 86ms, Memory: 5MB, Status: Passed - Codetree

---
+++ 251227/실수 받아 그대로 출력/enter-real-value-and-print.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        double N = double.Parse(Console.ReadLine());
+        Console.WriteLine($"{N:F2}");
+    }
+}
