6d616f2f8a802a73ef10da6eb1027fa39c9188ba
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-27 19:55:34 +0900

    Title: 점수 입출력, Time: 56ms, Memory: 6MB, Status: Passed - Codetree

---
+++ 251227/점수 입출력/enter-int-and-print-score.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Codetree
+{  
+    public static void Main()
+    {
+        string N = Console.ReadLine();
+        
+        Console.WriteLine($"Your score is {N} point.");
+    }
+}
