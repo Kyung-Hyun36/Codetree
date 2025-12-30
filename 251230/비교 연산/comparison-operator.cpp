60536dbc7f9409b9acce92bba06e3ca6d70a29ca
Kyung-Hyun36 <66782867+Kyung-Hyun36@users.noreply.github.com>
2025-12-30 11:02:41 +0900

    Title: 비교 연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251230/비교 연산/comparison-operator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A >= B){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    if (A > B){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    if (B >= A){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    if (B > A){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    if (A == B){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    if (A != B){
+        cout << 1 << "\n";
+    }else{
+        cout << 0 << "\n";
+    }
+
+    return 0;
+}
