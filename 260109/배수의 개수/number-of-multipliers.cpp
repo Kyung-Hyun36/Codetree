0884b6ab9ad91a8e1dbcf007502b312834b21ff8
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-09 10:20:27 +0900

    Title: 배수의 개수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260109/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int cnt_3 = 0, cnt_5 = 0;
+
+    for(int i = 0; i < 10; i++) cin >> arr[i];
+
+    for(int i : arr){
+        if(i % 3) cnt_3++;
+        if(i % 5) cnt_5++;
+    }
+
+    cout << cnt_3 << " " << cnt_5;
+
+    return 0;
+}
