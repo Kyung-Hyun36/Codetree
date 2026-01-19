7e74d10047455eac518311301100af593cef8dd6
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-01-19 15:12:13 +0900

    Title: 두 사람, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260119/두 사람/two-person.cpp
+++ 260119/두 사람/two-person.cpp
@@ -6,8 +6,7 @@ int main() {
     char gen1, gen2;
     cin >> age1 >> gen1 >> age2 >> gen2;
 
-    if(age1 >= 19 || age2 >= 19) cout << (gen1 == 'M' || gen2 == 'M');
-    else cout << 0;
+    cout << ((age1 >= 19 && gen1 == 'M') || (age2 >= 19 && gen2 == 'M'));
 
     return 0;
 }
