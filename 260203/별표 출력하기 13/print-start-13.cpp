e1b8b951ca58272c96958c9fda4f9700b3d1283a
김경현 <66782867+kyunghyun306@users.noreply.github.com>
2026-02-03 12:19:55 +0900

    Title: 별표 출력하기 13, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260203/별표 출력하기 13/print-start-13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n * 2 + 1, 0);
+
+    for(int i = 1; i < arr.size(); i += 2) arr[i] = n--;
+
+    for(int i = 2; i < arr.size(); i += 2) arr[i] = ++n;
+
+    for(int i = 1; i < arr.size(); i++){
+        for(int j = 1; j <= arr[i]; j++){
+            cout << "* ";
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
